875405fccb1062aeb239fb9e97789ed8497eac9f
Sjors Spoorendonk <sjors@netvlies.nl>
2017-04-18 16:48:29 +0200

    Updated rule

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = {
 		"comma-dangle": ["error", "never"],
 		"no-console": "off",
 		"space-before-function-paren": ["error", "always"],
-		"arrow-parens": ["error", "always"]
+		"arrow-parens": ["error", "always"],
+		"class-methods-use-this": "off"
 	}
 }
